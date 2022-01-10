2e29b1cf6fa87c9dc6c0c0b8a90b22de658c89c2
jaylim92 <90398720+jaylim92@users.noreply.github.com>
2022-01-10 20:26:38 +0900

    create chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,5 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
@@ -15,3 +17,12 @@ socket.addEventListener("close", () => {
 setTimeout(() => {
   socket.send("Hello from the Browser!!");
 }, 5000);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
