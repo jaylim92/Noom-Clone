6c2313a67b780d641ebf67e418e08bbe3a8e356a
jaylim92 <90398720+jaylim92@users.noreply.github.com>
2022-01-12 22:35:33 +0900

    Make chat room

--- src/server.js
+++ src/server.js
@@ -16,11 +16,12 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  socket.on("enter_room", (msg, done) => {
-    console.log(msg);
-    setTimeout(() => {
-      done();
-    }, 10000);
+  socket.onAny((event) => {
+    console.log(`socket event: ${event}`);
+  });
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
   });
 });
 
